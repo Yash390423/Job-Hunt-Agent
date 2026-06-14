0e991e6bd642355d0a1de694d883afe5b5b57ab6
Yash Pankaj Chaudhari <che240008038@iiti.ac.in>
2026-06-15 01:16:44 +0530

    remove some unneccesary files

--- usajobs_api.py
+++
@@ -1,7 +0,0 @@
-"""Legacy compatibility wrapper.
-
-This project now uses India job listings via Adzuna, but this module is kept
-so older imports do not break immediately.
-"""
-
-from utils.india_jobs_api import fetch_india_jobs as fetch_usajobs
